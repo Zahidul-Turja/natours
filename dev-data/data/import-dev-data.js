c149548fa5924521801a5db24422760733b10ef9
Zahidul-Turja <zahidulturja@gmail.com>
2024-03-08 19:01:13 +0600

    Importing script for dev-data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Tour = require("../../models/tourModel");
+
+dotenv.config({ path: "./config.env" });
+// ? The config file have to be read before importing the app
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Database connected successfully"));
+
+// ! Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+);
+
+// ! Import data into Database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("Data successfully loaded");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// ! Delete all Data from Database Collection
+const deleteData = async (req, res) => {
+  try {
+    await Tour.deleteMany();
+    console.log("Data deleted successfully!");
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
+
+// console.log(process.argv);
