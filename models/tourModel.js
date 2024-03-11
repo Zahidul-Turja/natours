6d1c4e2280601e67e21a63eabda6309d5628f352
Zahidul-Turja <zahidulturja@gmail.com>
2024-03-11 22:19:03 +0600

    mongoose validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 // eslint-disable-next-line import/no-extraneous-dependencies
 const slugify = require("slugify");
+// eslint-disable-next-line import/no-extraneous-dependencies
+// const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +11,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"],
       unique: true,
       trim: true,
+      maxlength: [40, "A tour name must have less or equal than 40 characters"],
+      minlength: [10, "A tour name must have more or equal than 10 characters"],
+      // validate: [validator.isAlpha, "Tour name must only contain characters"],
     },
     slug: String,
     duration: {
@@ -22,10 +27,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty level"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium or difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
     },
     ratingsQuantity: {
       type: Number,
@@ -35,7 +46,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // ! this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: "Discount price ({VALUE}) should be below regular price",
+      },
+    },
     summary: {
       type: String,
       trim: true,
