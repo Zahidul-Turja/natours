c2db038137c4bcdc4091d4ba89a80804bde2cf59
Zahidul-Turja <zahidulturja@gmail.com>
2024-05-25 12:17:45 +0600

    Preventing users from giving multiple reviews for one tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,9 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// ! One user should give only one review to one tour
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
   //   path: "tour",
